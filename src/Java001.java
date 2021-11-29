e6399578a0ea41527a1d073af69d222ec188288d
yh725 <yh725@kakao.com>
2021-11-29 22:45:07 +0900

    Java 001

---
+++ src/Java001.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public int solution(String str, char t){
+        int answer = 0;
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+        for(char x : str.toCharArray()){
+            if(t == x) answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        System.out.print(T.solution(str, c));
+    }
+}
