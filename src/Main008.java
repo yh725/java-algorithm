0777b4102bb5836a815194f99cd02852de384103
yh725 <yh725@kakao.com>
2021-12-05 21:52:22 +0900

    8. 유요한 팰린드롬

---
+++ src/Main008.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main008 {
+    public String solution(String str) {
+        String answer = "NO";
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+
+        String tmp = new StringBuilder(str).reverse().toString();
+        if(str.equals(tmp)) return "YES";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main008 T = new Main008();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+
+        System.out.print(T.solution(str));
+    }
+}
