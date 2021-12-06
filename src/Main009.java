fd581f56f8907459d99a14dfa241d50e534f9018
yh725 <yh725@kakao.com>
2021-12-06 21:35:14 +0900

    9. 숫자만 추출

---
+++ src/Main009.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Main009 {
+    public int solution(String str) {
+        //int answer = 0;
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            /*if(x >= 48 && x < 57) {
+                answer = answer * 10 + (x - 48);
+            }*/
+            if(Character.isDigit(x)) answer += String.valueOf(x);
+        }
+
+        return Integer.parseInt(answer);
+    }
+
+    public static void main(String[] args) {
+        Main009 T = new Main009();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.nextLine();
+
+        System.out.print(T.solution(str));
+    }
+}
