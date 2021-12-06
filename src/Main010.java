b7a8f63e00bb0e84d97631d6e8a0f96ae0b86b07
yh725 <yh725@kakao.com>
2021-12-06 22:13:40 +0900

    10. 가장 짧은 문자거리

---
+++ src/Main010.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Main010 {
+    public int[] solution(String str, char c) {
+        int[] answer = new int[str.length()];
+        int p = 1000;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+
+        p = 1000;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == c) p = 0;
+            else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main010 T = new Main010();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+
+        int i = 1;
+        for (int x : T.solution(str, c)) {
+            System.out.print(x);
+            if(T.solution(str, c).length > i){
+                i++;
+                System.out.print(" ");
+            }
+        }
+    }
+}
