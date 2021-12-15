b8a60f252d89cb83ba730cf3e5f3465dae17a98b
yh725 <yh725@kakao.com>
2021-12-15 23:58:32 +0900

    Array 8. 등수 구하기

---
+++ src/Main008.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main008 {
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] > arr[i]) cnt++;
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main008 T = new Main008();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
+}
